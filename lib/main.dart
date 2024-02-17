a5adcb272b02defd062bd9619f5dc6671aa668b0
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2024-02-17 19:39:13 +0530

    new slider, few check boxes for password validating and checking added. extract checkbox widget

--- lib/main.dart
+++ lib/main.dart
@@ -31,13 +31,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  double valueForSlider = 23;
   bool _numbers = false;
   bool _simple = false;
-  bool _capital = false;
-  bool _special = false;
-
+  bool _capital = true;
+  bool _special = true;
   int _counter = 0;
-
   void _incrementCounter() {
     setState(() {
       _counter++;
@@ -55,17 +54,77 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+            buildCheckboxListTile(
+                title: 'Numbers',
+                subtitle: 'Add numbers to password',
+                iconselect: Icon(Icons.numbers),
+                valueSelected: _numbers),
+            buildCheckboxListTile(
+                title: 'Simple Letters',
+                iconselect: Icon(Icons.abc_sharp),
+                subtitle: 'Add simple letters to password',
+                valueSelected: _simple),
+            buildCheckboxListTile(
+                title: 'Capital Letters',
+                iconselect: Icon(Icons.abc_sharp),
+                subtitle: 'Add capital letters to password',
+                valueSelected: _capital),
+            buildCheckboxListTile(
+                title: 'Special Characters',
+                iconselect: Icon(Icons.hdr_auto_sharp),
+                subtitle: 'Add special characters to password',
+                valueSelected: _special),
             Text(
-              '$_counter',
+              'You have $_numbers pushed the $_capital button this many times:',
+            ),
+            Slider.adaptive(
+                min: 5,
+                max: 55,
+                divisions: 50,
+                label: valueForSlider.round().toString(),
+                value: valueForSlider,
+                onChanged: ((double newValueOfSlider) {
+                  setState(() {
+                    valueForSlider = newValueOfSlider.round().toDouble();
+                  });
+                })),
+            Text(
+              '${valueForSlider.toInt()}',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
       ),
       // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  CheckboxListTile buildCheckboxListTile({
+    required String title,
+    required String subtitle,
+    required Icon iconselect,
+    required bool valueSelected,
+  }) {
+    return CheckboxListTile(
+      title: Text(title),
+      subtitle: Text(subtitle),
+      value: valueSelected,
+      secondary: iconselect,
+      onChanged: (bool? newValue) {
+        setState(
+          () {
+            if (title == 'Numbers') {
+              _numbers = newValue!;
+            } else if (title == 'Simple Letters') {
+              _simple = newValue!;
+            } else if (title == 'Capital Letters') {
+              _capital = newValue!;
+            } else if (title == 'Special Characters') {
+              _special = newValue!;
+            }
+          },
+        );
+      },
     );
   }
 }
